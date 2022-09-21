b6fe67b958b049db9c2111263988f6a5c20d060c
miralmag <miriam.almagro@gmail.com>
2022-09-21 10:08:53 +0200

    fix fetch url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const dataApi = (data) => {
-  return fetch('http://localhost:4000/card', {
+  return fetch('https://batwomen.herokuapp.com//card', {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
